7e4d566e254f68871be2e222fc762d6abec47906
storm00530 <rim00530@outlook.com>
2022-01-28 02:34:08 -0500

    pos fix

--- server/router/posRouter.js
+++ server/router/posRouter.js
@@ -1,34 +1,47 @@
 const Router = require("koa-router");
 const trackModel = require("../../models/trackModel");
-
+var i = 0;
 const router = new Router({
   prefix: "/pos",
 });
 
 function register(app) {
   router.post("/promotions", async (ctx) => {
+    i++;
     const body = ctx.request.body;
-    console.log("pos request", body);
-    // const trackData = await trackModel.find({})
-    // var total = 0;
-    // trackData.forEach(d => {
-    //   total += d.track;
-    // });
 
-    // console.log("track", total);
+    const trackData = await trackModel.find({});
+    var total = 0,
+      curr = {};
+    var balance = 0,
+      title = "";
+    trackData.forEach((d) => {
+      if (d.customer_email === body.customer_email) {
+        curr.track = d.track;
+        curr.name = d.customer_name;
+      }
+      total += d.track;
+    });
+    if (body.customer_id === null) {
+      balance = total;
+      title = "Total";
+    } else {
+      title = curr.name;
+      balance = curr.track || 0;
+    }
 
     ctx.status = 200;
     ctx.body = {
       type: "simple_action_list",
-      points_label: "Points balance",
-      points_balance: "23867",
+      points_label: "Purchases",
+      points_balance: balance,
       actions: [
         {
           type: "flat_discount",
-          title: "Add $5.00 discount",
-          description: "-1000 points",
-          action_id: "123ABC",
-          value: "5",
+          title: title,
+          description: "Track Purchase History",
+          action_id: "123ABC" + i,
+          value: "1",
         },
       ],
     };
